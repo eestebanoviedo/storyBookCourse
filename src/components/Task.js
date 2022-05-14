aefe589ef2e2925ffc9312242a27bfd12b325b24
Esteban <ooviedoesteban@gmail.com>
2022-05-14 17:37:45 -0300

    change task background to black

--- src/components/Task.js
+++ src/components/Task.js
@@ -27,7 +27,7 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
-          style={{ background: "red" }}
+          style={{ background: "black" }}
         />
       </div>
 
